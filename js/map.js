86151573031cb6372f2d0d36800d32032f8f621c
yohman <yohman@gmail.com>
2021-09-30 15:58:47 -0700

    changed basemap, added categorical colors

--- js/map.js
+++ js/map.js
@@ -14,9 +14,24 @@ $( document ).ready(function() {
 function createMap(){
 	map = L.map('map').setView([0,0],3);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	/*
+	
+		default (for now) is mapbox black and white satellite		
+	
+	*/ 
+	let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/yohman/ckon2lqfc00bu17nrdwdtsmke/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieW9obWFuIiwiYSI6IkxuRThfNFkifQ.u2xRJMiChx914U7mOZMiZw', 
+	{
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox/yohman</a>',
+		maxZoom: 18,
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: 'pk.eyJ1IjoieW9obWFuIiwiYSI6IkxuRThfNFkifQ.u2xRJMiChx914U7mOZMiZw'
 	}).addTo(map);
+
+
+	// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	// }).addTo(map);
 }
 
 // function to read csv data
@@ -36,17 +51,30 @@ function readCSV(){
 
 function mapCSV(data){
 
+	// create an array of distinct categories
+	const distinct_themes = [...new Set(data.data.map(item => item.Theme))]; 
+	const distinct_years = [...new Set(data.data.map(item => item.SourceYear))]; 
+	console.log(distinct_years)
+	// create an array of colors
+	// const colors = ['#88CCEE','#CC6677','#DDCC77','#117733','#332288','#AA4499','#44AA99','#999933','#882255','#661100','#6699CC','#888888']
+	let colors = d3.scale.category20().range()
+
 	// circle options
 	let circleOptions = {
 		radius: 7,
 		weight: 1,
 		color: 'white',
-		fillColor: 'dodgerblue',
+		// fillColor: 'dodgerblue',
 		fillOpacity: 1
 	}
 
 	// loop through each entry
 	data.data.forEach(function(item,index){
+
+		// where is this item's theme in the distinct_themes array?
+		array_position = distinct_themes.indexOf(item.Theme)
+		circleOptions.fillColor = colors[array_position]
+
 		// create a marker
 		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
 		.on('click',function(){
